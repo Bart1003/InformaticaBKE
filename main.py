7d02d94140076026706a0c28b31b328012793ba2
Bart van <57255021+Bart1003@users.noreply.github.com>
2022-03-07 23:14:05 +0000

    Gamestate verder bewerkt en speel functie aangemaakt

--- main.py
+++ main.py
@@ -15,22 +15,36 @@ class MyAgent(MLAgent):
     else:
       reward = 0
     return reward
-    
- 
-my_agent = load('MyAgent_3000')
-my_agent.learning = False
- 
-validation_agent = RandomAgent()
- 
-validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
- 
-plot_validation(validation_result)
 
-def parameters():
-  print("Bij een machine-learning agent kan het gedrag aangepast worden door middel van parameters te gebruiken. In het aangeleverde stukje code van Fundament zijn hiervoor al twee hyperparameters gebruikt; Alpha en Epsilon \nEpsilon bepaalt de frequentie waarmee de agent een nieuwe zet zal proberen te maken. De waarde ligt, net zoals bij Alpha, tussen de 0 en de 1. Hoe hoger het Epsilon getal is, bepaald de randomness van een zet. Dichterbij de 0 geeft bekendere zetten, dichterbij de 1 geeft meer random zetten. \nAlpha als parameter bepaalt de snelheid waarmee de agent nieuwe kennis vergaart. Oude kennis wordt sneller vervangen door de nieuwere kennis indien deze Alpha waarde dichterbij de 1 ligt.")
+def Spelen():
+
+  while True:
+    play_agent = input("vul de naam in van de agent waar tegen jij wilt spelen")
+    try:
+      my_agent = load(play_agent)
+    except:
+      print("Deze bestaat niet, selecteer alsjeblieft een uit de lijst van bestaande agenten")
+      continue
+    else: 
+      my_agent = load(play_agent)
+      break
+
+  my_agent.learning = False
+
+  while True:
+      teamkeuze = input("Wil je x of o zijn? (x begint altijd) \n")
+      if teamkeuze == "O":
+        start(player_x=my_agent) 
+        break 
+      elif teamkeuze == "X":
+        start(player_o=my_agent)
+        break
+
+
+
 
 def Agenttrainen():
-naam = input("Typ hier de naam van je agent")
+  naam = input("Typ hier de naam van je agent")
   
   train(my_agent, 5000)
 
@@ -45,4 +59,13 @@ while True:
   if keuzemenu == '2':
     Agenttrainen()
   if keuzemenu == '3':
+    Spelen()
+
+  play = input("Spel afsluiten? (ja/nee) \n")
+  if play == 'nee':
+    continue
+  else:
+    break
     
+def hyperparameters():
+  print("Bij een machine-learning agent kan het gedrag aangepast worden door middel van parameters te gebruiken. In het aangeleverde stukje code van Fundament zijn hiervoor al twee hyperparameters gebruikt; Alpha en Epsilon \nEpsilon bepaalt de frequentie waarmee de agent een nieuwe zet zal proberen te maken. De waarde ligt, net zoals bij Alpha, tussen de 0 en de 1. Hoe hoger het Epsilon getal is, bepaald de randomness van een zet. Dichterbij de 0 geeft bekendere zetten, dichterbij de 1 geeft meer random zetten. \nAlpha als parameter bepaalt de snelheid waarmee de agent nieuwe kennis vergaart. Oude kennis wordt sneller vervangen door de nieuwere kennis indien deze Alpha waarde dichterbij de 1 ligt.")
